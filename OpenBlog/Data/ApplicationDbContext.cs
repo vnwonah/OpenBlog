41ca6e5188fb87b6d664b447c78db3f8e49400a7
Vincent Nwonah <vnwonah@outlook.com>
2020-03-09 10:29:43 +0100

    Added Dockerfile

---
+++ OpenBlog/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OpenBlog.Entities;
+
+namespace OpenBlog.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ OpenBlog/Entities/Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OpenBlog.Entities
+{
+    public class Post
+    {
+        [Key]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Title { get; set; }
+        public string Slug { get; set; } 
+        public string Body { get; set; }
+    }
+}
